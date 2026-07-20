9b901f80c2cbcdb42b7dd5accbb4eb44cd679710
nwmarino <nwmarinoto@gmail.com>
2026-07-20 19:16:00 -0400

    Refractions

--- Material.h
+++ Material.h
@@ -50,3 +50,38 @@ public:
         return (dot(scattered.dir, rec.normal) > 0);
     }
 };
+
+class Dialectric : public Material {
+    double refraction_index = 0.0;
+
+public:
+    Dialectric(double refraction_index) : refraction_index(refraction_index) {}
+
+    bool scatter(const Ray& r_in, const HitRecord& rec, Color& attenuation, Ray& scattered) const override {
+        attenuation = Color(1.0, 1.0, 1.0);
+        double ri = rec.front_face ? (1.0 / refraction_index) : refraction_index;
+
+        vec3 unit_dir = unit(r_in.dir);
+        double cos_theta = std::fmin(dot(-unit_dir, rec.normal), 1.0);
+        double sin_theta = std::sqrt(1.0 - cos_theta * cos_theta);
+
+        bool cannot_refract = ri * sin_theta > 1.0;
+        vec3 dir = {};
+
+        if (cannot_refract || reflectance(cos_theta, ri) > randomDouble()) {
+            dir = reflect(unit_dir, rec.normal);
+        } else {
+            dir = refract(unit_dir, rec.normal, ri);
+        }
+
+        scattered = Ray(rec.p, dir);
+        return true;
+    }
+
+private:
+    static double reflectance(double cosine, double refraction_index) {
+        auto r0 = (1 - refraction_index) / (1 + refraction_index);
+        r0 = r0 * r0;
+        return r0 + (1 - r0) * std::pow((1 - cosine), 5);
+    }
+};
--- main.cpp
+++ main.cpp
@@ -15,19 +15,21 @@ int32_t main() {
 
     auto mat_ground = std::make_shared<Lambertian>(Color(0.8, 0.8, 0.0));
     auto mat_center = std::make_shared<Lambertian>(Color(0.1, 0.2, 0.5));
-    auto mat_left = std::make_shared<Metal>(Color(0.8, 0.8, 0.8), 0.3);
+    auto mat_left = std::make_shared<Dialectric>(1.50);
+    auto mat_bubble = std::make_shared<Dialectric>(1.00 / 1.50);
     auto mat_right = std::make_shared<Metal>(Color(0.8, 0.6, 0.2), 1.0);
 
     world.add(std::make_shared<Sphere>(point3(0.0, -100.5, -1.0), 100.0, mat_ground));
     world.add(std::make_shared<Sphere>(point3(0.0,    0.0, -1.2), 0.5, mat_center));
     world.add(std::make_shared<Sphere>(point3(-1.0, 0.0, -1.0), 0.5, mat_left));
+    world.add(std::make_shared<Sphere>(point3(-1.0, 0.0, -1.0), 0.4, mat_bubble));
     world.add(std::make_shared<Sphere>(point3(1.0, 0.0, -1.0), 0.5, mat_right));
-
+\
     Camera camera = {};
 
     camera.aspect_ratio = 16.0 / 9.0;
     camera.image_width = 720;
-    camera.samples_per_pixel = 16;
+    camera.samples_per_pixel = 100;
     camera.max_depth = 16;
 
     camera.render(world);
